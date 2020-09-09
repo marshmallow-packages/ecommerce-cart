534dbbcf7060015f4d898961d614ec9a17a86886
Stef van Esch <stef@marshmallow.dev>
2020-09-09 14:48:27 +0200

    PSR-4

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -15,7 +15,7 @@ class ServiceProvider extends BaseServiceProvider
     public function register()
     {
         $this->mergeConfigFrom(
-            __DIR__.'/config/cart.php', 'cart'
+            __DIR__.'/../config/cart.php', 'cart'
         );
     }
 
@@ -30,28 +30,28 @@ class ServiceProvider extends BaseServiceProvider
         app('router')->aliasMiddleware('cart', CartMiddleware::class);
 
         $this->publishes([
-            __DIR__.'/config/cart.php' => config_path('cart.php'),
+            __DIR__.'/../config/cart.php' => config_path('cart.php'),
         ]);
 
-        $this->loadRoutesFrom(__DIR__.'/routes.php');
-        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
-        $this->loadFactoriesFrom(__DIR__.'/database/factories');
-        $this->loadTranslationsFrom(__DIR__.'/resources/lang', 'ecommerce');
-        $this->loadViewsFrom(__DIR__.'/resources/views', 'ecommerce');
+        $this->loadRoutesFrom(__DIR__.'/../routes.php');
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->loadFactoriesFrom(__DIR__.'/../database/factories');
+        $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'ecommerce');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'ecommerce');
         $this->loadViewComponentsAs('ecommerce', [
             \Marshmallow\Ecommerce\Cart\View\Components\Cart::class,
         ]);
 
         $this->publishes([
-            __DIR__.'/resources/lang' => resource_path('lang/vendor/ecommerce/cart'),
+            __DIR__.'/../resources/lang' => resource_path('lang/vendor/ecommerce/cart'),
         ], 'translations');
 
         $this->publishes([
-            __DIR__.'/resources/views' => resource_path('views/vendor/ecommerce/cart'),
+            __DIR__.'/../resources/views' => resource_path('views/vendor/ecommerce/cart'),
         ], 'views');
 
         $this->publishes([
-            __DIR__.'/public' => public_path('vendor/marshmallow/ecommerce/cart'),
+            __DIR__.'/../public' => public_path('vendor/marshmallow/ecommerce/cart'),
         ], 'public');
 
         if ($this->app->runningInConsole()) {
