da6e668f37072e672947f3486bce5a2ebaf946ba
Stef van Esch <stef@marshmallow.dev>
2022-01-18 18:32:52 +0100

    Updated config

--- config/cart-discount.php
+++ config/cart-discount.php
@@ -2,17 +2,11 @@
 
 return [
     'voucher' => [
-        'min_ength' => 8,
+        'min_length' => 8,
         'exclude_rules' => ['Symbols', 'Lowercase', 'Similar'],
     ],
     'default' => [
-        'vat_rate' => 6,
-        'currency' => 2,
-    ],
-
-    'data_connectors' => [
-        'products' => \Marshmallow\Ecommerce\Cart\Helpers\DiscountProductSelector::class,
-        'product_categories' => \Marshmallow\Ecommerce\Cart\Helpers\DiscountProductCategorySelector::class,
-        'customers' => \Marshmallow\Ecommerce\Cart\Helpers\DiscountCustomerSelector::class,
+        'vat_rate' => env('DISCOUNT_DEFAULT_VAT_RATE_ID', 3),
+        'currency' => env('DISCOUNT_DEFAULT_CURRENCY_ID', 1),
     ],
 ];
