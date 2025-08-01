c8144bd0b4d69e1764c0d830bf41b0ff29d97a2c
Stef van Esch <stef@marshmallow.dev>
2025-08-01 11:35:58 +0200

    (fix) keep classes because the could be referenced in published config files

---
+++ src/Http/Livewire/ShoppingCart.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Marshmallow\Ecommerce\Cart\Http\Livewire;
+
+class ShoppingCart
+{
+    /**
+     * @deprecated This class is deprecated and will be removed in a future version.
+     */
+}
